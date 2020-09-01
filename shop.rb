58bcb3fd92ed22f085f9537c2a308cb73723a043
Emma Bindi <emma.bindi@outlook.com>
2020-09-01 15:26:48 +1000

    Before changing to sqlite3

--- shop.rb
+++ shop.rb
@@ -1,11 +1,13 @@
 require 'json'
+require 'csv'
 require_relative 'products'
 require "tty-prompt"
+require 'sqlite3'
 
 prompt = TTY::Prompt.new
 
-# Load a list of products from a file. 
-# List product details to the user.
+# DONE Load a list of products from a file. 
+# DONE List product details to the user.
 # Add products to a Shopping Cart. 
 # Apply promotional discounts. 
 # Calculate and display the total cost.
@@ -27,4 +29,26 @@ product_list.each do | item |
   options.push(item.name)
 end 
 
-prompt.select("Choose your item", options)
+begin
+  p "Hi, welcome to Bob's Bits the #1 CLI Shop"
+  p ""
+  # p "Menu:"
+  # menu_selection = prompt.select("Select a menu item", ["View All Products", "View Shopping Cart"])
+
+  # List product names so that user can select
+  item_to_add_to_cart = prompt.select("Select which items to add to cart", options)
+  shopping_cart = []
+  shopping_cart.push(item_to_add_to_cart)
+  p shopping_cart
+
+  CSV.generate do | csv |
+    csv << [:blue, 1]
+    csv << [:white, 2]
+  end
+  
+
+  
+  
+rescue => exception
+  puts "Sorry that didn't work, please try again"
+end

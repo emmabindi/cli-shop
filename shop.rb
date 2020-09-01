372f212d3ac44ae71bc0c1a7579e8a942e3797d5
Emma Bindi <emma.bindi@outlook.com>
2020-09-01 17:15:57 +1000

    A few diff ways to load the product file before deleting

--- shop.rb
+++ shop.rb
@@ -26,23 +26,59 @@ end
 
 def view_all_products
   prompt = TTY::Prompt.new
-  # Read products file 
-  file = File.read('./products.json')
-  # Parse json into hash
-  products_json = JSON.parse(file)
+
+
+  # # Read products file 
+  # file = File.read('./products.json')
+  # # Parse json into hash
+  # products_json = JSON.parse(file)
+
+  # shows but doesnt save:
+  # data_hash = JSON.parse(File.read('./products.json'))
+  # data_hash.map do | product |
+  #   puts "This is inside the map#{product}"
+  # end
+
+  file = File.open "./products.json"
+  data = JSON.load file
+
+  puts data
+  puts data[0]
+  puts data[1]["name"]
+
   # Display the products hash
-  product_list = []
-  products_json.each do | product |
-    row = product.to_hash
-    product_list.push(Product.new(row['uuid'], row['name'], row['price']))
-  end
-  # Print out each items name
-  options = []
-  product_list.each do | item |
-    options.push(item.name)
-  end 
-  # List product names so that user can select
-  item_to_add_to_cart = prompt.select("Select which items to add to cart", options)
+  # product_list = []
+  # products_json.each do | product |
+  #   row = product.to_hash
+  #   product_list.push(Product.new(row['uuid'], row['name'], row['price']))
+  # end
+  # Print out each items name for the menu selection
+  # options = []
+  # product_list.each do | item |
+  #   options.push(item.name)
+  # end 
+  # # List product names so that user can select
+  # item_to_add_to_cart = prompt.select("Select which items to add to cart", options)
+
+  # item_data_for_cart = product_list.select do |product |
+  #   item_to_add_to_cart == product.name
+  # end
+
+  # puts ""
+  # puts item_data_for_cart
+
+
+
+
+
+
+  # shopping_cart = []
+  
+  # def add_item_to_cart(item_to_add_to_cart)
+  #   shopping_cart.push(item_to_add_to_cart)
+  # end
+  # menu
+  # return shopping_cart
 end
 
 def view_shopping_cart
@@ -51,16 +87,11 @@ end
 
 begin
   menu
-
-  # shopping_cart = []
-  # shopping_cart.push(item_to_add_to_cart)
-  # p shopping_cart
-
   # CSV.generate do | csv |
   #   csv << [:blue, 1]
   #   csv << [:white, 2]
   # end
-  
+
   
 # rescue => exception
 #   puts "Sorry that didn't work, please try again"

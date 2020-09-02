fd06c0282b70bc91bc063943d4752e18ab7ab693
Emma Bindi <emma.bindi@outlook.com>
2020-09-02 16:11:00 +1000

    Create cart test not complete yet

---
+++ spec/cart_spec.rb
@@ -0,0 +1,8 @@
+# require 'rspec'
+
+# RSpec.describe Cart do
+#   let (:cart) { Product.new(123, "Gadget", 12.50)}
+
+#   https://mixandgo.com/learn/how-to-test-ruby-modules-with-rspec
+#   #
+# end

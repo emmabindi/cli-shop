81a45e14d22aba914290932fa820b3532b499794
Emma Bindi <emma.bindi@outlook.com>
2020-09-02 20:17:03 +1000

    README updated

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
+Running Instructions:
+
 ```
-$ git clone <this repo>
+git clone this repo
 $ gem install tty-prompt json
 $ ruby shop.rb
 ```
 
-Command Line Application
+To load a new product list:
 
-Functionality:
+- Load your product data into a json file named "products.json" and save at root of project folder (existing file is here already)
+
+#### Command Line Application: Ruby
+
+##### Required Functionality:
 
 - Load a list of products from a file.
 - List product details to the user.
 - Add products to a Shopping Cart.
 - Apply promotional discounts.
 - Calculate and display the total cost.
+
+---
+
+##### Design Methodology
+
+- DRY code principles
+- Concise & meaningful method names
+- Modularised methods (Shopping Cart actions separated)
+- Product Class utilised to create all shop product objects
+- Testing of app functionality through extensive manual testing
+- RSPEC testing of product class
--- app/cart.rb
+++ app/cart.rb
@@ -3,15 +3,19 @@ require 'tty-prompt'
 module Cart
   @shopping_cart = []
 
+  # Add the product selected to the shopping cart array
   def self.add_to_cart(product_for_cart)
     @shopping_cart.push(product_for_cart).flatten!
     puts `clear`
   end
 
   def self.view_shopping_cart
+    # apply promo discounts based on cart subtotal
     def self.discount_calc
+      # calculator subtotal of items in cart
       subtotal = @shopping_cart.map {|item| item["price"]}.sum
       @cart_data = {}
+      # track the discount for display & calc later on
       if subtotal >= 100.0 
         @cart_data[:discount] = 20
       elsif subtotal >= 50
@@ -22,15 +26,18 @@ module Cart
         @cart_data[:discount] = 0
       end
       @cart_data[:total] = (subtotal - (subtotal * @cart_data[:discount] / 100.00))
+      @cart_data[:savings] = subtotal - @cart_data[:total]
       return @cart_data
     end
+    # run the discount calculator
     discount_calc
+    # Output cart to the user
     puts ""
     puts "Products in Shopping Cart:"
     @shopping_cart.each_with_index do | item, i |
       puts "  #{i + 1}. #{item["name"]} - $#{'%.2f' % item["price"]}\n\n"
     end
-    puts "Discount applied: #{@cart_data[:discount]}%\n\n"
+    puts "Discount applied: #{@cart_data[:discount]}% (Total Savings $#{'%.2f' % @cart_data[:savings]}!)\n\n"
     puts "Total After Discount: $#{'%.2f' % @cart_data[:total]}\n\n"
   end
 end
